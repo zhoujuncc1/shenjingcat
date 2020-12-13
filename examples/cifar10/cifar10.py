d4146dda804ddfad7525355eaf1b34a4672e52d9
zhangluyan9 <39037304+zhangluyan9@users.noreply.github.com>
2020-12-13 19:04:24 +0800

    Update cifar10.py

--- examples/cifar10/cifar10.py
+++ examples/cifar10/cifar10.py
@@ -2,13 +2,14 @@ from __future__ import print_function
 import os
 os.environ['CUDA_LAUNCH_BLOCKING'] = "1"
 import argparse
+import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 from torchvision import datasets, transforms
 from torch.optim.lr_scheduler import StepLR
-from catSNN import spikeLayer, transfer_model, load_model, max_weight, normalize_weight, SpikeDataset ,fuse_bn_recursively 
+from catSNN import spikeLayer, transfer_model, load_model, max_weight, normalize_weight, SpikeDataset ,fuse_bn_recursively, fuse_module
 from utils import to_tensor
 
 class AddGaussianNoise(object):
@@ -21,6 +22,13 @@ class AddGaussianNoise(object):
     
     def __repr__(self):
         return self.__class__.__name__ + '(mean={0}, std={1})'.format(self.mean, self.std)
+
+def Initialize_trainable_pooling(feature):
+    datashape = feature.shape
+    for i in range(datashape[0]):
+        for j in range(datashape[1]):
+            feature[i][j] = torch.Tensor(0.25*np.ones((2,2)))
+    return nn.Parameter(feature, requires_grad=True)
 
 def train(args, model, device, train_loader, optimizer, epoch):
     model.train()
@@ -52,12 +60,14 @@ def test(model, device, test_loader):
             test_loss += F.cross_entropy(output, target, reduction='sum').item()  # sum up batch loss
             pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
             correct += pred.eq(target.view_as(pred)).sum().item()
+        
 
     test_loss /= len(test_loader.dataset)
 
     print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
         test_loss, correct, len(test_loader.dataset),
         100. * correct / len(test_loader.dataset)))
+    return correct
 
 
 
@@ -86,8 +96,11 @@ def main():
                         help='For Saving the current Model')
     parser.add_argument('--resume', type=str, default=None, metavar='RESUME',
                         help='Resume model from checkpoint')
-    parser.add_argument('--T', type=int, default=60, metavar='N',
+    parser.add_argument('--T', type=int, default=1000, metavar='N',
                         help='SNN time window')
+    parser.add_argument('--k', type=int, default=5, metavar='N',
+                        help='Data augmentation')
+
     args = parser.parse_args()
     use_cuda = not args.no_cuda and torch.cuda.is_available()
 
@@ -101,8 +114,6 @@ def main():
                        'pin_memory': True,
                        'shuffle': True},
                      )
-    mean = [0.4913997551666284, 0.48215855929893703, 0.4465309133731618]
-    std = [0.24703225141799082, 0.24348516474564, 0.26158783926049628]
 
     transform_train = transforms.Compose([
         transforms.RandomCrop(32, padding=6),
@@ -110,66 +121,69 @@ def main():
         transforms.ToTensor(),
         AddGaussianNoise(std=0.01)
         ])
-    im_aug = transforms.Compose([
-        #transforms.ColorJitter(brightness=0.5, contrast=0.5, hue=0.5),
+
+    transform_test = transforms.Compose([
+        transforms.ToTensor()
+        ])
+
+    trainset = datasets.CIFAR10(
+        root='./data', train=True, download=True, transform=transform_train)
+    train_loader_ = torch.utils.data.DataLoader(trainset, batch_size=512, shuffle=True)
+       
+    for i in range(args.k):
+
+        im_aug = transforms.Compose([
         transforms.RandomRotation(10),
         transforms.RandomCrop(32, padding = 6),
         transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),
         AddGaussianNoise(std=0.01)
         ])
-
-    transform_test = transforms.Compose([
-        transforms.ToTensor()
-        #transforms.Normalize(mean, std)
-        ])
-
-    trainset = datasets.CIFAR10(
-        root='./data', train=True, download=True, transform=transform_train)
-    
-    for i in range(100):
         trainset = trainset + datasets.CIFAR10(root='./data', train=True, download=True, transform=im_aug)
-        
+    
     train_loader = torch.utils.data.DataLoader(
-        trainset, batch_size=128, shuffle=True)
+        trainset, batch_size=256, shuffle=True)
 
     testset = datasets.CIFAR10(
-        root='./data', train=False, download=True, transform=transform_test)
+        root='./data', train=False, download=False, transform=transform_test)
     test_loader = torch.utils.data.DataLoader(
         testset, batch_size=100, shuffle=False)
 
     snn_dataset = SpikeDataset(testset, T = args.T)
     snn_loader = torch.utils.data.DataLoader(snn_dataset, batch_size=10, shuffle=False)
 
-    from models.vgg import VGG, CatVGG
+    from models.vgg import VGG, VGG_,CatVGG,CatVGG_
+    
+    model = VGG('VGG19', clamp_max=1, quantize_bit=32,bias =True).to(device)
+    snn_model = CatVGG('VGG19', args.T,bias =True).to(device)
 
-    model = VGG('VGG19', clamp_max=1, quantize_bit=32).to(device)
-    snn_model = CatVGG('VGG19', args.T).to(device)
+    #Trainable pooling
+    #model = VGG_('VGG19_', clamp_max=1, quantize_bit=32,bias =True).to(device)
+    #snn_model = CatVGG_('VGG19_', args.T,bias =True).to(device)
+
     if args.resume != None:
         model.load_state_dict(torch.load(args.resume), strict=False)
-        load_model(torch.load(args.resume), model)
-        load_model(torch.load(args.resume), snn_model)
+    
     optimizer = optim.Adam(model.parameters(), lr=args.lr)
     scheduler = StepLR(optimizer, step_size=1, gamma=args.gamma)
+
+    correct_ = 0
     for epoch in range(1, args.epochs + 1):
         train(args, model, device, train_loader, optimizer, epoch)
-        #test(model, device, train_loader)
-        test(model, device, test_loader)
-
-        #transfer_model(model, snn_model)
-        #test(snn_model, device, snn_loader)
-        if args.save_model:
-            torch.save(model.state_dict(), "cifar_cnn_19.pt")
-        
+        test(model, device, train_loader_)
+        correct = test(model, device, test_loader)
+        if correct>correct_:
+            correct_ = correct
+            if args.save_model:
+                torch.save(model.state_dict(), "cifar_cnn_19.pt")
         scheduler.step()
-    #test(model, device, train_loader)
-    test(model, device, test_loader)
+
+    model = fuse_bn_recursively(model)
     transfer_model(model, snn_model)
     with torch.no_grad():
         normalize_weight(snn_model.features, quantize_bit=8)
     test(snn_model, device, snn_loader)
-    if args.save_model:
-        torch.save(model.state_dict(), "cifar_cnn_19.pt")
+
 
 
 if __name__ == '__main__':
