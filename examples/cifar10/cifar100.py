2b483f4e090545aa9d429726ca3e68384a8e9e9e
zhangluyan9 <39037304+zhangluyan9@users.noreply.github.com>
2021-01-20 21:34:51 +0800

    Update Cifar100

---
+++ examples/cifar10/cifar100.py
@@ -0,0 +1,249 @@
+from __future__ import print_function
+import os
+import torch.utils.model_zoo as model_zoo
+from collections import OrderedDict
+os.environ['CUDA_LAUNCH_BLOCKING'] = "1"
+import argparse
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import datasets, transforms
+from torch.optim.lr_scheduler import StepLR
+from catSNN import spikeLayer, transfer_model, SpikeDataset
+from utils import to_tensor
+
+def fuse_bn_sequential(block):
+    if not isinstance(block, nn.Sequential):
+        return block
+    stack = []
+    for m in block.children():
+        if isinstance(m, nn.BatchNorm2d):
+            if isinstance(stack[-1], nn.Conv2d):
+                bn_st_dict = m.state_dict()
+                conv_st_dict = stack[-1].state_dict()
+
+                eps = m.eps
+                mu = bn_st_dict['running_mean']
+                var = bn_st_dict['running_var']
+
+                if 'weight' in bn_st_dict:
+                    gamma = bn_st_dict['weight']
+                else:
+                    gamma = torch.ones(mu.size(0)).float().to(mu.device)
+                #gamma = bn_st_dict['weight']
+
+                if 'bias' in bn_st_dict:
+                    beta = bn_st_dict['bias']
+                else:
+                    beta = torch.zeros(gamma.size(0)).float().to(gamma.device)
+
+                # Conv params
+                W = conv_st_dict['weight']
+                if 'bias' in conv_st_dict:
+                    bias = conv_st_dict['bias']
+                else:
+                    bias = torch.zeros(W.size(0)).float().to(gamma.device)
+
+                denom = torch.sqrt(var + eps)
+                b = beta - gamma.mul(mu).div(denom)
+                A = gamma.div(denom)
+                bias *= A
+                A = A.expand_as(W.transpose(0, -1)).transpose(0, -1)
+
+                W.mul_(A)
+                bias.add_(b)
+
+                stack[-1].weight.data.copy_(W)
+                if stack[-1].bias is None:
+                    stack[-1].bias = torch.nn.Parameter(bias)
+                else:
+                    stack[-1].bias.data.copy_(bias)
+
+        else:
+            stack.append(m)
+
+    if len(stack) > 1:
+        return nn.Sequential(*stack)
+    else:
+        return stack[0]
+
+
+def fuse_bn_recursively(model):
+    for module_name in model._modules:
+        model._modules[module_name] = fuse_bn_sequential(model._modules[module_name])
+        if len(model._modules[module_name]._modules) > 0:
+            fuse_bn_recursively(model._modules[module_name])
+
+    return model
+
+class AddQuantization(object):
+    def __init__(self, min=0., max=1.):
+        self.min = min
+        self.max = max
+        
+    def __call__(self, tensor):
+        #return tensor + torch.randn(tensor.size()) * self.std + self.mean
+        return torch.div(torch.floor(torch.mul(tensor, 10.0)), 10.0)
+
+
+class AddGaussianNoise(object):
+    def __init__(self, mean=0., std=1.):
+        self.std = std
+        self.mean = mean
+
+    def __call__(self, tensor):
+        return tensor + torch.randn(tensor.size()) * self.std + self.mean
+
+    def __repr__(self):
+        return self.__class__.__name__ + '(mean={0}, std={1})'.format(self.mean, self.std)
+
+
+def train(args, model, device, train_loader, optimizer, epoch):
+    model.train()
+    for batch_idx, (data, target) in enumerate(train_loader):
+        data, target = data.to(device), target.to(device)
+        #onehot = torch.nn.functional.one_hot(target, 10)
+        optimizer.zero_grad()
+        output = model(data)
+        loss = F.cross_entropy(output, target)
+        loss.backward()
+        optimizer.step()
+        if batch_idx % args.log_interval == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                       100. * batch_idx / len(train_loader), loss.item()))
+            if args.dry_run:
+                break
+
+
+def test(model, device, test_loader):
+    model.eval()
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            #onehot = torch.nn.functional.one_hot(target, 10)
+            output = model(data)
+            test_loss += F.cross_entropy(output, target, reduction='sum').item()  # sum up batch loss
+            pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+            correct += pred.eq(target.view_as(pred)).sum().item()
+            print(pred.eq(target.view_as(pred)).sum().item())
+
+    test_loss /= len(test_loader.dataset)
+
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+        test_loss, correct, len(test_loader.dataset),
+        100. * correct / len(test_loader.dataset)))
+    return correct
+
+
+def main():
+    # Training settings
+    parser = argparse.ArgumentParser(description='PyTorch Cifar100 LeNet Example')
+    parser.add_argument('--batch-size', type=int, default=80, metavar='N',
+                        help='input batch size for training (default: 64)')
+    parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
+                        help='input batch size for testing (default: 1000)')
+    parser.add_argument('--epochs', type=int, default=0, metavar='N',
+                        help='number of epochs to train (default: 14)')
+    parser.add_argument('--lr', type=float, default=1e-3, metavar='LR',
+                        help='learning rate (default: 1)')
+    parser.add_argument('--gamma', type=float, default=0.7, metavar='M',
+                        help='Learning rate step gamma (default: 0.7)')
+    parser.add_argument('--no-cuda', action='store_true', default=False,
+                        help='disables CUDA training')
+    parser.add_argument('--dry-run', action='store_true', default=False,
+                        help='quickly check a single pass')
+    parser.add_argument('--seed', type=int, default=1, metavar='S',
+                        help='random seed (default: 1)')
+    parser.add_argument('--log-interval', type=int, default=10, metavar='N',
+                        help='how many batches to wait before logging training status')
+    parser.add_argument('--save-model', action='store_true', default=True,
+                        help='For Saving the current Model')
+    parser.add_argument('--resume', type=str, default=None, metavar='RESUME',
+                        help='Resume model from checkpoint')
+    parser.add_argument('--T', type=int, default=150, metavar='N',
+                        help='SNN time window')
+    parser.add_argument('--k', type=int, default=0, metavar='N',
+                        help='Data augmentation')
+    args = parser.parse_args()
+    use_cuda = not args.no_cuda and torch.cuda.is_available()
+
+    torch.manual_seed(args.seed)
+
+    device = torch.device("cuda" if use_cuda else "cpu")
+
+    kwargs = {'batch_size': args.batch_size}
+    if use_cuda:
+        kwargs.update({'num_workers': 1,
+                       'pin_memory': True,
+                       'shuffle': True},
+                      )
+    mean = [0.4913997551666284, 0.48215855929893703, 0.4465309133731618]
+    std = [0.24703225141799082, 0.24348516474564, 0.26158783926049628]
+
+    transform_train = transforms.Compose([
+        transforms.RandomCrop(32, padding=4),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        AddGaussianNoise(std=0.01),
+        ])
+
+    transform_test = transforms.Compose([
+        transforms.ToTensor(),
+    ])
+
+
+    trainset = datasets.CIFAR100(root='./data', train=True, download=True, transform=transform_train)
+    train_loader_ = torch.utils.data.DataLoader(trainset, batch_size=512+256, shuffle=True)
+
+    for i in range(args.k):
+        im_aug = transforms.Compose([
+        transforms.RandomRotation(10),
+        transforms.RandomCrop(32, padding = 6),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        AddGaussianNoise(std=0.01),
+        ])
+        trainset = trainset + datasets.CIFAR100(root='./data', train=True, download=True, transform=im_aug)
+
+    train_loader = torch.utils.data.DataLoader(trainset, batch_size=512+256, shuffle=True)
+
+    testset = datasets.CIFAR100(root='./data', train=False, download=True, transform=transform_test)
+    test_loader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False)
+
+
+    snn_dataset = SpikeDataset(testset, T=args.T)
+    snn_loader = torch.utils.data.DataLoader(snn_dataset, **kwargs)
+
+    from models.vgg_ import CatVGG,VGG_
+    model = VGG_('o', clamp_max=1.0, bias = False).to(device)
+    #model.load_state_dict(torch.load("cifar100_vggo_.pt"), strict=False)
+    snn_model = CatVGG('o', args.T, is_noise=False, bias = True).to(device)
+
+    if args.resume != None:
+        model.load_state_dict(torch.load(args.resume))
+
+    optimizer = optim.Adam(model.parameters(), lr=args.lr)
+    scheduler = StepLR(optimizer, step_size=1, gamma=args.gamma)
+    correct_ = 0
+    for epoch in range(1, args.epochs + 1):
+        train(args, model, device, train_loader, optimizer, epoch)
+        test(model, device, train_loader_)
+        correct = test(model, device, test_loader)
+        if correct>correct_:
+            correct_ = correct
+            #torch.save(model.state_dict(), "cifar100_vggo.pt")
+        scheduler.step()
+
+    model = fuse_bn_recursively(model)
+    transfer_model(model, snn_model)
+    test(snn_model, device, snn_loader)
+    
+
+
+
+if __name__ == '__main__':
+    main()
