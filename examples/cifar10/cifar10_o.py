a1088d34bfe30a95776ad786c1a2cf3379a17181
zhangluyan9 <39037304+zhangluyan9@users.noreply.github.com>
2021-02-20 13:43:54 +0800

    Add files via upload

---
+++ examples/cifar10/cifar10_o.py
@@ -0,0 +1,203 @@
+from __future__ import print_function
+import os
+#os.environ['CUDA_LAUNCH_BLOCKING'] = "0"
+import argparse
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import datasets, transforms
+from torch.optim.lr_scheduler import StepLR
+from utils import to_tensor
+import time
+from catSNN import spikeLayer, transfer_model, load_model, max_weight, normalize_weight, SpikeDataset ,fuse_bn_recursively, fuse_module
+from models.vgg import VGG_o, CatVGG_o
+
+
+def change_shape(feature):
+    datashape = feature.shape
+    for i in range(datashape[0]):
+        for j in range(datashape[1]):
+            feature[i][j] = torch.Tensor(0.25*np.ones((2,2)))
+    return nn.Parameter(feature, requires_grad=True)
+
+class AddGaussianNoise(object):
+    def __init__(self, mean=0., std=1.):
+        self.std = std
+        self.mean = mean
+        
+    def __call__(self, tensor):
+        return tensor + torch.randn(tensor.size()) * self.std + self.mean
+    
+    def __repr__(self):
+        return self.__class__.__name__ + '(mean={0}, std={1})'.format(self.mean, self.std)
+
+def Initialize_trainable_pooling(feature):
+    datashape = feature.shape
+    for i in range(datashape[0]):
+        for j in range(datashape[1]):
+            feature[i][j] = torch.Tensor(0.25*np.ones((2,2)))
+    return nn.Parameter(feature, requires_grad=True)
+
+def train(args, model, device, train_loader, optimizer, epoch):
+    model.train()
+    for batch_idx, (data, target) in enumerate(train_loader):
+        data, target = data.to(device), target.to(device)
+        optimizer.zero_grad()
+        output = model(data)
+        loss = F.cross_entropy(output, target)
+        loss.backward()
+        optimizer.step()
+        if batch_idx % args.log_interval == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                100. * batch_idx / len(train_loader), loss.item()))
+            if args.dry_run:
+                break
+
+
+def test(model, device, test_loader):
+    model.eval()
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            test_loss += F.cross_entropy(output, target, reduction='sum').item()  # sum up batch loss
+            pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+            correct += pred.eq(target.view_as(pred)).sum().item()
+
+    test_loss /= len(test_loader.dataset)
+
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+        test_loss, correct, len(test_loader.dataset),
+        100. * correct / len(test_loader.dataset)))
+    return correct
+
+
+
+def main():
+    # Training settings
+    parser = argparse.ArgumentParser(description='PyTorch Cifar10 LeNet Example')
+    parser.add_argument('--batch-size', type=int, default=64, metavar='N',
+                        help='input batch size for training (default: 64)')
+    parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
+                        help='input batch size for testing (default: 1000)')
+    parser.add_argument('--epochs', type=int, default=10, metavar='N',
+                        help='number of epochs to train (default: 14)')
+    parser.add_argument('--lr', type=float, default=1e-3, metavar='LR',
+                        help='learning rate (default: 1)')
+    parser.add_argument('--gamma', type=float, default=0.7, metavar='M',
+                        help='Learning rate step gamma (default: 0.7)')
+    parser.add_argument('--no-cuda', action='store_true', default=False,
+                        help='disables CUDA training')
+    parser.add_argument('--dry-run', action='store_true', default=False,
+                        help='quickly check a single pass')
+    parser.add_argument('--seed', type=int, default=1, metavar='S',
+                        help='random seed (default: 1)')
+    parser.add_argument('--log-interval', type=int, default=10, metavar='N',
+                        help='how many batches to wait before logging training status')
+    parser.add_argument('--save-model', action='store_true', default=False,
+                        help='For Saving the current Model')
+    parser.add_argument('--resume', type=str, default=None, metavar='RESUME',
+                        help='Resume model from checkpoint')
+    parser.add_argument('--T', type=int, default=400, metavar='N',
+                        help='SNN time window')
+    parser.add_argument('--k', type=int, default=50, metavar='N',
+                        help='Data augmentation')
+
+    args = parser.parse_args()
+    use_cuda = not args.no_cuda and torch.cuda.is_available()
+
+    torch.manual_seed(args.seed)
+
+    device = torch.device("cuda" if use_cuda else "cpu")
+
+    kwargs = {'batch_size': args.batch_size}
+    if use_cuda:
+        kwargs.update({'num_workers': 1,
+                       'pin_memory': True,
+                       'shuffle': True},
+                     )
+    normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                     std=[0.229, 0.224, 0.225])
+
+    transform_train = transforms.Compose([
+        
+        transforms.RandomCrop(32, padding=4),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        AddGaussianNoise(std=0.01),
+        ])
+
+    transform_test = transforms.Compose([
+        transforms.ToTensor(),
+        ])
+
+    trainset = datasets.CIFAR10(
+        root='./data', train=True, download=True, transform=transform_train)
+    train_loader_ = torch.utils.data.DataLoader(trainset, batch_size=512, shuffle=True)
+       
+    for i in range(args.k):
+
+        im_aug = transforms.Compose([
+        transforms.RandomRotation(15),
+        transforms.RandomCrop(32, padding = 6),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        AddGaussianNoise(std=0.01),
+        ])
+        trainset = trainset + datasets.CIFAR10(root='./data', train=True, download=True, transform=im_aug)
+
+    for i in range(args.k):
+        im_aug = transforms.Compose([
+        transforms.RandomRotation(10),
+        transforms.RandomCrop(32, padding = 6),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        AddGaussianNoise(std=0.01),
+        ])
+        trainset = trainset + datasets.CIFAR10(root='./data', train=True, download=True, transform=im_aug)
+   
+
+    
+    train_loader = torch.utils.data.DataLoader(
+       trainset, batch_size=512+256, shuffle=True)
+
+    testset = datasets.CIFAR10(
+        root='./data', train=False, download=False, transform=transform_test)
+
+    test_loader = torch.utils.data.DataLoader(
+        testset, batch_size=15*8, shuffle=False)
+
+
+    snn_dataset = SpikeDataset(testset, T = args.T,theta = 1)
+    snn_loader = torch.utils.data.DataLoader(snn_dataset, batch_size=25, shuffle=False)
+
+    
+    model = VGG_o('o', clamp_max=1,bias =True).to(device)
+    snn_model = CatVGG_o('o', args.T,bias =True).to(device)
+    #model.load_state_dict(torch.load("cifar_cnn_o_10_9396.pt"), strict=False)
+
+    optimizer = optim.Adam(model.parameters(), lr=args.lr)
+    scheduler = StepLR(optimizer, step_size=1, gamma=args.gamma)
+    test(model, device, test_loader)
+    correct_ = 0
+    for epoch in range(1, args.epochs + 1):
+        train(args, model, device, train_loader, optimizer, epoch)
+        test(model, device, train_loader_)
+        correct = test(model, device, test_loader)
+        if correct>correct_:
+            correct_ = correct
+        scheduler.step()
+    model = fuse_bn_recursively(model)
+    transfer_model(model, snn_model)
+    test(snn_model, device, snn_loader)
+    
+    
+
+
+if __name__ == '__main__':
+    main()
