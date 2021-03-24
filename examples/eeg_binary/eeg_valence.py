77f5ac2696e957accc9bdfeb03d87e8abe5c7c43
zhangluyan9 <39037304+zhangluyan9@users.noreply.github.com>
2021-03-24 23:16:09 +0800

    Update eeg_valence.py

--- examples/eeg_binary/eeg_valence.py
+++ examples/eeg_binary/eeg_valence.py
@@ -10,8 +10,10 @@ import torch.nn.functional as F
 import torch.optim as optim
 from torchvision import datasets, transforms
 from torch.optim.lr_scheduler import StepLR
-from catSNN import spikeLayer, transfer_model, SpikeDataset ,load_model, fuse_module
+from catSNN import spikeLayer, transfer_model, SpikeDataset ,load_model, fuse_module,normalize_weight
 from scipy import signal
+from sklearn.metrics import confusion_matrix
+
 class AddGaussianNoise(object):
     def __init__(self, mean=0., std=1.):
         self.std = std
@@ -33,45 +35,45 @@ class Dataset(object):
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(1, 64, 5, 1, 0,bias=True)
-        self.Bn1 = nn.BatchNorm2d(64)
-        self.conv2 = nn.Conv2d(64, 64, 5, 1,0,bias=True)
-        self.Bn2 = nn.BatchNorm2d(64)
-        self.conv3 = nn.Conv2d(64, 128, 5, 1, 0,bias=True)
-        self.Bn3 = nn.BatchNorm2d(128)
+        self.conv1 = nn.Conv2d(1, 16, 5, 1, 0,bias=True)
+        self.Bn1 = nn.BatchNorm2d(16)
+        self.conv2 = nn.Conv2d(16, 32, 5, 1,0,bias=True)
+        self.Bn2 = nn.BatchNorm2d(32)
+        self.conv3 = nn.Conv2d(32, 32, 5, 1, 0,bias=True)
+        self.Bn3 = nn.BatchNorm2d(32)
 
         self.dropout1 = nn.Dropout(0.1)
         self.dropout2 = nn.Dropout(0.1)
         self.dropout3 = nn.Dropout(0.1)
         self.dropout4 = nn.Dropout(0.5)
 
-        self.fc1 = nn.Linear(128* 4*4,128, bias=True)
+        self.fc1 = nn.Linear(512,128, bias=True)
         self.fc2 = nn.Linear(128, 2, bias=True)
 
     def forward(self, x):
         x = self.conv1(x)
         x = self.Bn1(x)
         x = torch.clamp(x, min=0, max=1)
-        x = torch.div(torch.ceil(torch.mul(x,100)),100)
+        #x = torch.div(torch.ceil(torch.mul(x,100)),100)
 
         x = self.conv2(x)
         x = self.Bn2(x)
         x = torch.clamp(x, min=0, max=1)
-        x = torch.div(torch.ceil(torch.mul(x,100)),100)
+        #x = torch.div(torch.ceil(torch.mul(x,100)),100)
 
         x = F.avg_pool2d(x, 2)
 
         x = self.conv3(x)
         x = self.Bn3(x)
         x = torch.clamp(x, min=0, max=1)
-        x = torch.div(torch.ceil(torch.mul(x,100)),100)
+        #x = torch.div(torch.ceil(torch.mul(x,100)),100)
 
         x = F.avg_pool2d(x, 2)
 
         x = torch.flatten(x, 1)
         x = self.fc1(x)
         x = torch.clamp(x, min=0, max=1)
-        x = torch.div(torch.ceil(torch.mul(x,100)),100)
+        #x = torch.div(torch.ceil(torch.mul(x,100)),100)
         output = self.fc2(x)
 
         return output
@@ -85,12 +87,12 @@ class CatNet(nn.Module):
         snn = spikeLayer(T)
         self.snn=snn
 
-        self.conv1 = snn.conv(1, 64, 5, 1,0,bias=True)
-        self.conv2 = snn.conv(64, 64, 5, 1,0, bias=True)
-        self.conv3 = snn.conv(64, 128,5,1,0, bias=True)
+        self.conv1 = snn.conv(1, 16, 5, 1,0,bias=True)
+        self.conv2 = snn.conv(16, 32, 5, 1,0, bias=True)
+        self.conv3 = snn.conv(32, 32,5,1,0, bias=True)
         self.pool1 = snn.pool(2)
         self.pool2 = snn.pool(2)
-        self.fc1 = snn.dense((4,4,128), 128, bias=True)
+        self.fc1 = snn.dense((4,4,32), 128, bias=True)
         self.fc2 = snn.dense(128, 2, bias=True)
 
     def forward(self, x):
@@ -151,13 +153,74 @@ def test(model, device, test_loader):
         test_loss, correct, len(test_loader.dataset),
         100. * correct / len(test_loader.dataset)))
     return correct
+def test_conf(model, device, test_loader):
+    model.eval()
+    test_loss = 0
+    correct = 0
+    pre_l = []
+    t = []
+    pre_l_ = []
+    pre_l_1 = []
+    #all_pre = []
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            data = data.type(torch.cuda.FloatTensor)
+            target = target.type(torch.cuda.LongTensor) 
+            output = model(data)
+            test_loss += F.cross_entropy(output, target, reduction='sum').item()  # sum up batch loss
+            pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+            pre_l.append(pred)
+            t.append(target)
+            correct += pred.eq(target.view_as(pred)).sum().item()
+
+    test_loss /= len(test_loader.dataset)
+    pre_l = np.array(pre_l)
+    for item in pre_l[0]:
+        pre_l_.append(item[0].cpu().numpy())
+    for i in range(len(pre_l_)):
+        pre_l_[i] = int(pre_l_[i])
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+        test_loss, correct, len(test_loader.dataset),
+        100. * correct / len(test_loader.dataset)))
+    return pre_l_
+
+def test_conf_s(model, device, test_loader):
+    model.eval()
+    test_loss = 0
+    correct = 0
+    pre_l = []
+    t = []
+    pre_l_ = np.ones(1832)
+    pre_l_1 = []
+    #all_pre = []
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            data = data.type(torch.cuda.FloatTensor)
+            target = target.type(torch.cuda.LongTensor) 
+            output = model(data)
+            test_loss += F.cross_entropy(output, target, reduction='sum').item()  # sum up batch loss
+            pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+            for item in pred:
+                pre_l.append(int(item))
+            t.append(target)
+            correct += pred.eq(target.view_as(pred)).sum().item()
+
+    test_loss /= len(test_loader.dataset)
+    pre_l = np.array(pre_l)
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+        test_loss, correct, len(test_loader.dataset),
+        100. * correct / len(test_loader.dataset)))
+    #print(pre_l)
+    return pre_l
 
 
 
 def main():
     # Training settings
     parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
-    parser.add_argument('--batch-size', type=int, default=2, metavar='N',
+    parser.add_argument('--batch-size', type=int, default=1, metavar='N',
                         help='input batch size for training (default: 64)')
     parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
                         help='input batch size for testing (default: 1000)')
@@ -177,7 +240,7 @@ def main():
                         help='how many batches to wait before logging training status')
     parser.add_argument('--save-model', action='store_true', default=False,
                         help='For Saving the current Model')
-    parser.add_argument('--T', type=int, default=160, metavar='N',
+    parser.add_argument('--T', type=int, default=100, metavar='N',
                         help='SNN time window')
     parser.add_argument('--resume', type=str, default=None, metavar='RESUME',
                         help='Resume model from checkpoint')
@@ -194,7 +257,10 @@ def main():
     X_train_ = f['X_train_']
     y_train_ = f['y_train_']
     X_test = f['X_test']
+    
     y_test = f['y_test']
+    y_test_ = y_test
+
     X_train_ = torch.FloatTensor(X_train_)
     y_train_ = torch.FloatTensor(y_train_)
     X_test = torch.FloatTensor(X_test)
@@ -212,13 +278,13 @@ def main():
     torch_dataset_train = torch.utils.data.TensorDataset(X_train_, y_train_)
     torch_dataset_test = torch.utils.data.TensorDataset(X_test, y_test)
     snn_dataset = SpikeDataset(torch_dataset_test, T = args.T)
-    train_loader = torch.utils.data.DataLoader(torch_dataset_train,shuffle=True,batch_size=512)
-    test_loader = torch.utils.data.DataLoader(torch_dataset_test, shuffle=False,batch_size=64)
-    snn_loader = torch.utils.data.DataLoader(snn_dataset, shuffle=False,batch_size=16)
+    train_loader = torch.utils.data.DataLoader(torch_dataset_train,shuffle=True,batch_size=128)
+    test_loader = torch.utils.data.DataLoader(torch_dataset_test, shuffle=False,batch_size=1832)
+    snn_loader = torch.utils.data.DataLoader(snn_dataset, shuffle=False,batch_size=1)
 
     model = Net().to(device)
     snn_model = CatNet(args.T).to(device)
-
+    #"v0126_.pt"
     if args.resume != None:
         load_model(torch.load(args.resume), model)
 
@@ -234,16 +300,23 @@ def main():
         if Acc_>Acc:
             Acc = Acc_
             fuse_module(model)
-            #torch.save(model.state_dict(), "eeg_1201_3_layers_final_1.pt")
+            torch.save(model.state_dict(), "v0126_3.pt")
 
         scheduler.step()
     print(Acc)
-    fuse_module(model)
-    test(model, device, test_loader)
+    pre_ = test_conf(model, device, test_loader)
+    true_ = y_test_
 
+    print(confusion_matrix(true_, pre_))
+
+    fuse_module(model)
     transfer_model(model, snn_model)
-    test(snn_model, device, snn_loader)
-    
+
+    for param_tensor in snn_model.state_dict():
+        print(param_tensor, "\t", snn_model.state_dict()[param_tensor].size())
+
+    pre_s = test_conf_s(snn_model, device, snn_loader)
+    print(confusion_matrix(true_, pre_s))
 
 if __name__ == '__main__':
     main()
