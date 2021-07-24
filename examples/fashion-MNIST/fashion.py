cc5c60d9a5a0174cee1a8d807ca2954bb9f2a831
zhangluyan9 <39037304+zhangluyan9@users.noreply.github.com>
2021-07-24 12:51:36 +0800

    Update training parameters for easy usage.

--- examples/fashion-MNIST/fashion.py
+++ examples/fashion-MNIST/fashion.py
@@ -55,57 +55,54 @@ class Net(nn.Module):
         x = self.conv1(x)
         x = self.Bn1(x)
         x = torch.clamp(x, min=0, max=1)
-        x = torch.div(torch.ceil(torch.mul(x,10)),10)
+        #Please add Q function during retraining
+        #x = torch.div(torch.ceil(torch.mul(x,10)),10)
 
         x = self.conv2(x)
         x = self.Bn2(x)
         x = torch.clamp(x, min=0, max=1)
+        #Please add Q function during retraining
+        #x = torch.div(torch.ceil(torch.mul(x,10)),10)
 
-        #x = F.avg_pool2d(x, 2)
-        #x = self.dropout1(x)
-        x = torch.div(torch.ceil(torch.mul(x,10)),10)
         x = self.conv3(x)
         x = self.Bn3(x)
         x = torch.clamp(x, min=0, max=1)
-        x = torch.div(torch.ceil(torch.mul(x,10)),10)
+        #Please add Q function during retraining
+        #x = torch.div(torch.ceil(torch.mul(x,10)),10)
         #x = self.dropout1(x)
         
         x = self.conv4(x)
         x = self.Bn4(x)
         x = torch.clamp(x, min=0, max=1)
-        x = torch.div(torch.ceil(torch.mul(x,10)),10)
+        #Please add Q function during retraining
+        #x = torch.div(torch.ceil(torch.mul(x,10)),10)
 
         x = self.conv5(x)
         x = self.Bn5(x)
         x = torch.clamp(x, min=0, max=1)
-        x = torch.div(torch.ceil(torch.mul(x,10)),10)
+        #Please add Q function during retraining
+        #x = torch.div(torch.ceil(torch.mul(x,10)),10)
 
         #x = self.dropout3(x)
 
         x = self.conv6(x)
         x = self.Bn6(x)
         x = torch.clamp(x, min=0, max=1)
-        x = torch.div(torch.ceil(torch.mul(x,10)),10)
+        #Please add Q function during retraining
+        #x = torch.div(torch.ceil(torch.mul(x,10)),10)
 
         #x = self.dropout2(x)
 
         x = self.conv7(x)
         x = self.Bn7(x)
         x = torch.clamp(x, min=0, max=1)
-        x = torch.div(torch.ceil(torch.mul(x,10)),10)
+        #Please add Q function during retraining
+        #x = torch.div(torch.ceil(torch.mul(x,10)),10)
 
         
         x = torch.flatten(x, 1)
 
         x = self.fc1(x)
-        x = torch.clamp(x, min=0, max=1)
-        #x = torch.div(torch.ceil(torch.mul(x,50)),50)
-        #x = self.dropout3(x)
-
-        #output = self.fc2(x)
-        #output= torch.clamp(output, min=0, max=1)
-        #x = torch.div(torch.ceil(torch.mul(x,50)),50)
-
         return x
 
 
@@ -139,7 +136,7 @@ class CatNet(nn.Module):
         x = self.snn.spike(self.conv5(x))
         x = self.snn.spike(self.conv6(x))
         x = self.snn.spike(self.conv7(x))
-        x = self.snn.spike(self.fc1(x))
+        x = self.fc1(x)
         return self.snn.sum_spikes(x)/self.T
 
 def train(args, model, device, train_loader, optimizer, epoch):
@@ -181,11 +178,11 @@ def test(model, device, test_loader):
 def main():
     # Training settings
     parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
-    parser.add_argument('--batch-size', type=int, default=64, metavar='N',
+    parser.add_argument('--batch-size', type=int, default=512, metavar='N',
                         help='input batch size for training (default: 64)')
     parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
                         help='input batch size for testing (default: 1000)')
-    parser.add_argument('--epochs', type=int, default=3, metavar='N',
+    parser.add_argument('--epochs', type=int, default=10, metavar='N',
                         help='number of epochs to train (default: 14)')
     parser.add_argument('--lr', type=float, default=1, metavar='LR',
                         help='learning rate (default: 1.0)')
@@ -230,7 +227,7 @@ def main():
     dataset1 = datasets.MNIST('../data', train=True, download=True,
                        transform=transform_train)
     
-    for i in range(1000):
+    for i in range(30):
         transform_train_1 = transforms.Compose([
 
             transforms.RandomRotation(10),
@@ -244,11 +241,9 @@ def main():
     dataset2 = datasets.MNIST('../data', train=False,
                        transform=transform)
     snn_dataset = SpikeDataset(dataset2, T = args.T)
-    #print(type(dataset1[0][0]))
     train_loader = torch.utils.data.DataLoader(dataset1,**kwargs)
     
     test_loader = torch.utils.data.DataLoader(dataset2, **kwargs)
-    #print(test_loader[0])
     snn_loader = torch.utils.data.DataLoader(snn_dataset, **kwargs)
 
     model = Net().to(device)
@@ -261,17 +256,19 @@ def main():
     optimizer = optim.Adadelta(model.parameters(), lr=args.lr)
 
     scheduler = StepLR(optimizer, step_size=1, gamma=args.gamma)
-    ACC = 9944
+    ACC = 0
     for epoch in range(1, args.epochs + 1):
         train(args, model, device, train_loader, optimizer, epoch)
         ACC_ = test(model, device, test_loader)
         if ACC_>ACC or ACC_ == ACC:
             ACC = ACC_
-            fuse_module(model)
-            transfer_model(model, snn_model)
-            test(snn_model, device, snn_loader)
+            torch.save(model.state_dict(), "mnist_pretrained.pt")
         
         scheduler.step()
+    # After retraining with Q function, you can transfer ANN to SNN.    
+    fuse_module(model)
+    transfer_model(model, snn_model)
+    test(snn_model, device, snn_loader)
 
     #if args.save_model:
 
